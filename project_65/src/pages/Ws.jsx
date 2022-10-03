1b20acb20d31e676faf893cd82b90ebcc877669e
Rueangsak <77095351+Rueangsak@users.noreply.github.com>
2022-10-03 19:21:40 +0700

    Create Ws.jsx

---
+++ project_65/src/pages/Ws.jsx
@@ -0,0 +1,12 @@
+import { Navigation } from "../components/navigation";
+
+const Ws = () => {
+  return (
+    <div>
+    <Navigation></Navigation>
+      <h1>rueangsak</h1>
+    </div>
+  );
+};
+
+export default Ws;
