f6386ec89871e74efe67118c106b0e0fa4a648e1
Rueangsak <77095351+Rueangsak@users.noreply.github.com>
2022-10-07 19:24:14 +0700

    Create presentation.jsx

---
+++ project_65/src/components/work/presentation.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+import './presentation.css'
+
+const Presentation = (props) => {
+  return (
+    <div className="presentation-container">
+      <div className="presentation-container1">
+        <div className="presentation-container2">
+          <button className="presentation-button button">{props.button}</button>
+        </div>
+        <div className="presentation-container3">
+          <div className="presentation-container4">
+            <button className="presentation-button1 button">
+              {props.button1}
+            </button>
+          </div>
+          <div className="presentation-container5"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Presentation.defaultProps = {
+  button: 'My Presentation',
+  button1: '+ New presentation',
+}
+
+Presentation.propTypes = {
+  button: PropTypes.string,
+  button1: PropTypes.string,
+}
+
+export default Presentation
