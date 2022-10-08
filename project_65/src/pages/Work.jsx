84a8bc5fa2637146ec3be9df88f391cef14c4e17
Rueangsak <77095351+Rueangsak@users.noreply.github.com>
2022-10-08 17:30:55 +0700

    Update Work.jsx

--- project_65/src/pages/Work.jsx
+++ project_65/src/pages/Work.jsx
@@ -1,15 +1,19 @@
 import Presentation from '../components/work/presentation'
 import Navbar1 from '../components/work/navbar1'
-import './page.css'
+import './work.css'
+import Add from '../components/add/Add';
 
 const Work = () => {
   return (
     <div> 
       <Navbar1 rootClassName="navbar1-root-class-name"></Navbar1>
       <Presentation></Presentation>
+      <Add></Add>
     </div>
   );
 };
+
+
 
 
 export default Work;
