f0dc65b82467416df030d0f085f0acf981fe2f60
Rueangsak <77095351+Rueangsak@users.noreply.github.com>
2022-10-08 18:28:22 +0700

    Update TodoItem.jsx

--- project_65/src/components/add/TodoItem.jsx
+++ project_65/src/components/add/TodoItem.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 const TodoItem = ({ todo, onEditClick, onDeleteClick }) => {
     return (
-        <li key={todo.id}>
+        <div className='todo'>
+        <li className='li' key={todo.id}>
             {todo.text}
             {" "}
+            <div className='edit'>
+            <button>Open</button>
             <button onClick={() => onEditClick(todo)}>Edit</button>
-            <button onClick={() => onDeleteClick(todo.id)}>X</button>
-        </li>
+            <button onClick={() => onDeleteClick(todo.id)}>Delete</button>
+            </div>
+        </li></div>
     )
 }
 
